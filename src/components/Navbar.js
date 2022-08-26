7c01697d7267e46298c3891ee8ff56d541c6ef4f
Gokulaa-krishnaa <66513639+Gokulaa-krishnaa@users.noreply.github.com>
2022-08-26 16:07:11 +0530

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,31 +2,48 @@ import { useRef } from "react";
 const Navbar = () => {
 
     const navbar = useRef();
+    const mobilenavbar = useRef();
     const hamburger = () => {
-        const burger = document.getElementById("hamburger-btn");
-        const menu = document.getElementById("menu");
-    
-        burger.addEventListener('click',() => {
           if (menu.classList.contains('hidden')){
             menu.classList.remove('hidden');  
           } else{
             menu.classList.add('hidden');
           }
-        })
-    }
-    console.log(window.pageYOffset)
+        }
+      
+    
+    
+    const menu = document.getElementById("menu");
+    const closeburger = (() => {
+      if (menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');  
+      } else{
+        menu.classList.add('hidden');
+      }
+    })
+    
     window.onscroll = () =>{
       if (window.pageYOffset >= 320){
       navbar.current.style.position = 'fixed';
+      mobilenavbar.current.style.position =' fixed';
+      mobilenavbar.current.style.top=0;
       navbar.current.style.top = 0;}
-      else
-      navbar.current.style.position = 'relative';
-      // if(window.pageYOffset)
+      else{
+        navbar.current.style.position = 'relative';
+        mobilenavbar.current.style.position = 'relative';
+      }
+     
     }
-    // console.log(navbar.current.offsetTop)
-    // if(navbar.current.offsetTop<0){
-    //   navbar.current.style.position = 'fixed';
+    // window.onscroll = () =>{
+    //   if (window.pageYOffset >= 320){
+    //   
+    //   mobilenavbar.current.style.top = 0;}
+    //   else{
+    //     ;
+    //   }
+     
     // }
+    
 
     return(
         <div >
@@ -44,7 +61,7 @@ const Navbar = () => {
                     </div>
         </div>
 
-        <div className=" w-full p-2 flex items-center justify-center lg:hidden" id="hamburger-btn">
+        <div className=" w-full p-2 flex items-center bg-white justify-center lg:hidden z-10" id="hamburger-btn" ref={mobilenavbar}>
             <button onClick={hamburger}
                                 type="button"
                                 className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
@@ -57,17 +74,18 @@ const Navbar = () => {
                                 </svg>
             </button>  
             <div className='flex w-full'></div>   
-            <button className='justify-items-end px-8 py-1 text-sm  bg-[#331748] hover:bg-blue-700  text-white rounded-2xl'>REGISTER</button>         
+            <a href="#register-id" ><button className='justify-items-end px-8 py-1 text-sm  bg-[#331748] hover:bg-blue-700  text-white rounded-2xl'>REGISTER</button></a>
         </div>
         
-        <div className='grid justify-items-start grid-rows-6 hidden' id="menu">
-            <a href="#aboutinst" className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">About</a>
-            <a href="#committee-side-id" className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Adviosy committee</a>
-            <a href="#topics-of-interest-id" className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Topics Of Interest</a>
-            <a href="#dates-to-remember-id" className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Timeline</a>
-            <a href="#guidelines-id" className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Guidelines</a> 
-            <a href="#speakerid" className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Speakers</a>
-            <a href="#contact-us" className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Contact Us</a>
+        <div className=' fixed  grid justify-items-start w-full  h-screen grid-rows-8 hidden top-0 left-0  bg-white/80 z-20' id="menu"  >
+            <button className="block mx-12 mt-4 text-base font-semibold text-[#331748]  hover:text-hoverpink" onClick={closeburger}> close  </button>
+            <a href="#aboutinst" onClick={closeburger} className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">About</a>
+            <a href="#committee-side-id" onClick={closeburger} className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Adviosy committee</a>
+            <a href="#topics-of-interest-id" onClick={closeburger} className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Topics Of Interest</a>
+            <a href="#dates-to-remember-id" onClick={closeburger} className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Timeline</a>
+            <a href="#guidelines-id" onClick={closeburger} className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Guidelines</a> 
+            <a href="#speakerid" onClick={closeburger} className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Speakers</a>
+            <a href="#contact-us" onClick={closeburger} className="block mx-12 mt-4  text-base  font-semibold text-[#331748]  hover:text-hoverpink">Contact Us</a>
         </div>        
         </div>
     )
